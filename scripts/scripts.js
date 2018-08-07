dd4d66ab867e306088ccf31e301f5aa1ffbfe388
Tremell Lowell <tremelljlowell@gmail.com>
2018-08-07 08:18:33 -0400

    added timestamp but keep getting error

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,6 +11,15 @@ $("#submit").click(function(){
         console.log(bugs);
            bugs.shift($("dead").val());
         });   
+
+function getTimeStamp() {
+    let now = new Date();
+    return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear() + " " + now.getHours() + ':'
+    + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now.getSeconds()) : (now.getSeconds())));
+     }
+     function setTime() {
+         document.getElementById('field').value = getTimeStamp();
+     }
 });
 
 
